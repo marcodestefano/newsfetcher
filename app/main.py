54336889be9f9baccec1aedde72d904d007594d6
Marco De Stefano <destmark@gmail.com>
2024-05-22 14:36:51 +0200

    updated requirements

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import google.generativeai as geminiai
 import uvloop
 import os
 import json
@@ -8,7 +9,8 @@ from fastapi.responses import JSONResponse, StreamingResponse
 from gnews import GNews
 from newspaper import Article
 from dotenv import load_dotenv
-from ai_utils import generate_summary_with_ai
+from openai import OpenAI
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 load_dotenv()
 
@@ -16,8 +18,13 @@ PORT = int(os.getenv('PORT', 8000))
 TIMEOUT = int(os.getenv('TIMEOUT', 60 * 60 * 12))
 ARTICLES = int(os.getenv('ARTICLES', 5))
 MAX_ARTICLES = int(os.getenv('MAX_ARTICLES', 15))
-MIN_ARTICLES = 1
 LANGUAGE = os.getenv('LANGUAGE','it') 
+MIN_ARTICLES = 1
+OPENAI_AI = 'openai'
+GEMINI_AI = 'gemini'
+OPENAI_DEFAULT_MODEL = 'gpt-3.5-turbo-0125'
+GEMINI_DEFAULT_MODEL = 'gemini-1.5-flash-latest'
+
 
 cached_google_news = None
 cache_expiration = datetime.now()
@@ -71,6 +78,33 @@ async def fetch_article_content(article_url, model, ai, aikey):
     except Exception as e:
         print("Error generating summary with AI:", e)
     return article_text
+
+async def generate_summary_with_ai(article_text, model, ai, aikey):
+    prompt = f"Summarize the article in a maximum of 5 paragraphs, beginning each with a relevant emoji. Ensure the summary is in the same language as the article: {article_text}"
+    prompt = prompt.replace('\n', ' ').replace('\r', '')
+    summary = None
+    if ai == OPENAI_AI:
+        openai_client = OpenAI(api_key=aikey)
+        response = openai_client.chat.completions.create(
+            model=model or OPENAI_DEFAULT_MODEL,
+            messages=[{"role": "system", "content": prompt}]
+        )
+        summary = response.choices[0].message.content
+    elif ai == GEMINI_AI:
+        geminiai.configure(api_key=aikey)
+        model=model or GEMINI_DEFAULT_MODEL
+        geminimodel = geminiai.GenerativeModel(model_name=model)
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
+        }
+        response = geminimodel.generate_content(prompt, safety_settings = safety_settings)
+        summary = response.text
+    else:
+        print(f"Unsupported AI provider: {ai}")
+    return summary
 
 @app.get("/", response_class=JSONResponse)
 async def default():
